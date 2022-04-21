6375ba40c4ab2b7a45fce12de0c2199029011f64
CIRELLI SOFIA <46645556@ort>
2022-04-21 09:00:54 -0300

    Initial commit

---
+++ Assets/Scripts/While_01.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class While_01 : MonoBehaviour
+{
+    public int counter;
+    public int counterMax;
+    public Text txt_display;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (counter < counterMax)
+        {
+            Debug.Log(counter);
+            counter++;
+        }
+        txt_display.text = ("Termino el While");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
